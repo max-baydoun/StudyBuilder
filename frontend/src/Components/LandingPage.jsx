f8c6a8774f4168f80c22e26b5ec9215d0faf14e0
Mohamad Baydoun <mx.baydoun@gmail.com>
2024-12-12 14:44:31 +1100

    Added file to fix images and managed to make progress on landing page logo

--- frontend/src/Components/LandingPage.jsx
+++ frontend/src/Components/LandingPage.jsx
@@ -1,4 +1,4 @@
-import {Box, Grid2, Typography} from '@mui/material'
+import {Box, Grid2} from '@mui/material'
 import SB from '../../assets/yep.png'
 
 function LandingPage() {
@@ -6,9 +6,18 @@ function LandingPage() {
   return (
         <Box id="Landing Page Box" sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', color: 'black', height: '100vh'}}>
 
-            <Grid2 id="Main Grid Landing Page" container columns={12} sx={{borderRadius: '20px', width: '100%', height: '100%', background: 'linear-gradient(to top, #2c3e50 0%, #2c3e50 40%, black 70%, black 100%)', display: 'inline-block'}}>
-                <Grid2 size={12} id="Logo" sx={{width: '100%', height: '40%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-                    <Box id="image" component="img" sx={{width: '300px', height: '300px', background: 'radial-gradient(closest-side ellipse at 50% 50%, rgba(255, 255, 255, 1) 0%, rgba(115, 230, 255, 0.5) 60%, rgba(0, 0, 0, 0) 100%)'}} src={SB}/>
+            <Grid2 id="Main Grid Landing Page" container columns={12} sx={{borderRadius: '20px', width: '100%', height: '100%', background: 'linear-gradient(to top, #2c3e50 5%, black 30%, black 100%)', display: 'inline-block'}}>
+                <Grid2 size={12} id="Logo" sx={{width: '100%', height: '80%', display: 'flex',  justifyContent: 'center', alignItems: 'center'}}>
+                    <Box id="image" component="img" sx={{
+                        width: '500px',
+                        height: '500px',
+                        fill:'red',
+                        background: 'radial-gradient(50% 50% ellipse at 50% 50%, rgba(255, 255, 255, 1) 0%, rgba(19,207,228, 1) 50%, rgba(0, 0, 0, 0) 100%)',
+                        border: '1px solid gray',
+                        borderRadius: '100%',
+                        boxShadow: '-0px -0px 40px 10px rgba(19,207,228, 1)'
+                    }}
+                        src={SB}/>
                 </Grid2> 
                 <Grid2 size={12} id="Study Builder Text" sx={{fontSize: '100px', color: 'white', display: 'flex', justifyContent: 'center', alignItems: 'top'}}>
                     Study Builder
