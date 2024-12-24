f807f5cb8a8309d7b0f4806b7e9413bc3c9942eb
Mohamad Baydoun <mx.baydoun@gmail.com>
2024-12-24 15:01:04 +1100

    Using psycopg2 instead of  psycopg

--- backend/src/decorators/db.py
+++ backend/src/decorators/db.py
@@ -1,18 +1,16 @@
 from functools import wraps
-#import sqlite3
-import psycopg
+import psycopg2
 
 def dbDecorator(func):
     @wraps(func)
     def inner(*args, **kwargs):
         res = None
-
         try:
-            conn = psycopg.connect("postgres://0193f68a-8365-75f3-be1c-e73785c2a4b6:7fea3964-b650-4961-8741-16c2d39edaea@us-west-2.db.thenile.dev/nile_sky_ocean", autocommit=True)
+            conn = psycopg2.connect("postgres://0193f68a-8365-75f3-be1c-e73785c2a4b6:7fea3964-b650-4961-8741-16c2d39edaea@us-west-2.db.thenile.dev/nile_sky_ocean")
+            conn.autocommit = True
             cur = conn.cursor()
-
             res = func(cur, *args, **kwargs)
-        except psycopg.Error as e:
+        except psycopg2.Error as e:
             print(f'Postgres error: {str(e)}')
             raise
         finally:
